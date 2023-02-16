ce0ce19f3b565fba9d78a26eccbf20a6ab048e51
DebbyAsh <dilorey35@gmail.com>
2023-02-16 13:41:07 -0800

    Task 6 Not Sizing

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -1,15 +1,14 @@
 #include <stdio.h>
 /**
- * main - Start of program
+ * main - we will start here
  * Return: Always 0 (success)
  */
 int main(void)
 {
-	printf("Size of a char:%zu byte(s)\n", (unsigned long)sizeof(char));
-	printf("Size of an int:%zu byte(s)\n", (unsigned long)sizeof(int));
-	printf("Size of a long int:%zu byte(s)\n", (unsigned long)sizeof(long int));
-	printf("Size of a long long int:%zu byte(s)\n",(unsigned long) sizeof(long long int));
-	printf("Size of a float:%zu byte(s)", (unsigned long)sizeof(float));
-
+	printf("Size of a char:%lu byte(s)\n", (unsigned long)sizeof(char));
+	printf("Size of an int:%lu byte(s)\n", (unsigned long)sizeof(int));
+	printf("Size of a long int:%lu byte(s)\n", (unsigned long)sizeof(long int));
+	printf("Size of a long long int:%lu byte(s)\n", (unsigned long)sizeof(long long int));
+	printf("Size of a float:%lu byte(s)\n", (unsigned long)sizeof(float));
 	return (0);
 }
