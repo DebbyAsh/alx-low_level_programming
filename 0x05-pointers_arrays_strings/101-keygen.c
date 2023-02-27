4be9653fe1164d53bd60b534e4b8b6a617573053
DebbyAsh <dilorey35@gmail.com>
2023-02-27 15:16:08 -0800

    Task 11

--- 0x05-pointers_arrays_strings/101-keygen.c
+++ 0x05-pointers_arrays_strings/101-keygen.c
@@ -32,7 +32,7 @@ int main(void)
 		}
 		for (index = 0 ; password[index]; index++)
 		{
-			if (password[iindex] >= (33 + diff_half1))
+			if (password[index] >= (33 + diff_half1))
 			{
 				password[index] -= diff_half1;
 				break;
