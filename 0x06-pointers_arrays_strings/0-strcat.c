d00cdba4609a9c29c93f7de02f5bbcb37967fad8
DebbyAsh <dilorey35@gmail.com>
2023-03-01 13:37:51 -0800

    Task 0

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -7,21 +7,15 @@
  */
 char *_strcat(char *dest, char *src)
 {
+	int destl = 0;
+	int srcl = 0;
 	int i;
-	int j;
 
-	i = 0;
-	while (dest[1] != '\0')
-	{
-		i++;
-	}
-	j = 0;
-	while (src[j] != '\0')
-	{
-		dest[i] = src[j];
-		i++;
-		j++;
-	}
-	dest[i] = '\0';
+	for (i = 0 ; dest[i] != '\0' ; i++)
+		destl++;
+	for (i = 0 ; src[i] != '\0' ; i++)
+		srcl++;
+	for (i = 0 ; i <= srcl ; i++)
+		dest[destl + i] = src[i];
 	return (dest);
 }
---
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -0,0 +1,11 @@
+#include "main.h"
+/**
+ * _strncpy - copies a string
+ * @dest: destination input
+ * @src: source input
+ * @n: number of byte
+ */
+char *_strncpy(char *dest, char *src, int n)
+{
+
+}
