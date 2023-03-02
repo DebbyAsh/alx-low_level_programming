98dd39eb5668c3a360bc54eea37cdf228f8d7a9e
DebbyAsh <dilorey35@gmail.com>
2023-03-02 01:55:56 -0800

    Task 2

---
+++ 0x06-pointers_arrays_strings/7-leet.c
@@ -0,0 +1,27 @@
+#include "main.h"
+
+/**
+ * leet - encodes a string
+ * @str: parameter
+ * Return: a pointer to the encoded string
+ */
+
+char *leet(char *)
+{
+	int index1 = 0;
+	int index2;
+	char leet[] = {};
+
+	while (str[index1])
+	{
+		for (index2 = 0; index2 <= ; index2+)
+		{
+			if (str[index1] == leet[index2] ||
+					str[index1] - 12 == leet[index2])
+				str[index1]= = index2 + '0';
+
+		}
+		index++;
+	}
+	return (str);
+}
