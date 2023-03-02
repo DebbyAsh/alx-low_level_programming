38231254bcdbd2c0b41f1855439dc78a69f7aa0a
DebbyAsh <dilorey35@gmail.com>
2023-03-02 02:42:54 -0800

    Task 7

--- 0x06-pointers_arrays_strings/7-leet.c
+++ 0x06-pointers_arrays_strings/7-leet.c
@@ -6,22 +6,22 @@
  * Return: a pointer to the encoded string
  */
 
-char *leet(char *)
+char *leet(char *str)
 {
 	int index1 = 0;
 	int index2;
-	char leet[] = {};
+	char leet[8] = {'O', 'L', '?', 'E', 'A', '?', '?', 'T'};
 
 	while (str[index1])
 	{
-		for (index2 = 0; index2 <= ; index2+)
+		for (index2 = 0; index2 <= 7 ; index2++)
 		{
 			if (str[index1] == leet[index2] ||
-					str[index1] - 12 == leet[index2])
-				str[index1]= = index2 + '0';
+					str[index1] - 32 == leet[index2])
+				str[index1] = index2 + '0';
 
 		}
-		index++;
+		index1++;
 	}
 	return (str);
 }
