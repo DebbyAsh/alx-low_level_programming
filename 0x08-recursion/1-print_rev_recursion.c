957da41451d2d1d37d2c9726a59e430164287e0e
DebbyAsh <dilorey35@gmail.com>
2023-03-08 14:20:56 -0800

    Task 1

--- 0x08-recursion/1-print_rev_recursion.c
+++ 0x08-recursion/1-print_rev_recursion.c
@@ -2,11 +2,15 @@
 
 /**
  * _print_rev_recursion - prints a string in reverse
- * @s: string 
- * Return: string 
+ * @s: string
  */
 
 void _print_rev_recursion(char *s)
 {
+	if (*s)
+	{
+		_print_rev_recursion(s + 1);
+		_putchar(*s);
+	}
 
 }
