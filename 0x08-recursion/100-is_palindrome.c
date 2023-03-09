b8d82316f38546603298542fb97216cca700ddef
DebbyAsh <dilorey35@gmail.com>
2023-03-08 18:54:55 -0800

    Task 7

--- 0x08-recursion/100-is_palindrome.c
+++ 0x08-recursion/100-is_palindrome.c
@@ -1,13 +1,35 @@
 #include "main.h"
 
 /**
+ *palindrome - function that indicates if a number is a palindrome
+ *@s: pointer to string
+ *@p: position
+ *Return: 1 if palindrome and 0 otherwise
+ */
+
+int palindrome(char *s, int p)
+{
+	if (p < 1)
+	{
+		return (1);
+	}
+	if (*s == *(s + p))
+	{
+		return (palindrome(s + 1, p - 2));
+	}
+	return (0);
+}
+
+/**
  *is_palindrome - function that indicates if a number is a palindrome
  *@s: string
- *Return : 1 if palindrome and 0 otherwise
+ *Return: 1 if palindrome and 0 otherwise
  */
 
 int is_palindrome(char *s)
 {
+	int len = _strlen_recursion(s);
 
+	return (palindrome(s, len - 1));
 }
 
