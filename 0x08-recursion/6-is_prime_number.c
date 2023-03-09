502e26ec6a31cd6555d2d5e176686a62d009eadd
DebbyAsh <dilorey35@gmail.com>
2023-03-08 18:42:35 -0800

    Task 6

--- 0x08-recursion/6-is_prime_number.c
+++ 0x08-recursion/6-is_prime_number.c
@@ -1,12 +1,20 @@
 #include "main.h"
 
 /**
- * is_prime_number - function that indicates a prime number 
+ * is_prime_number - function that indicates a prime number
  * @n: number
  * Return: 1 when prime and 0 otherwise
  */
 
 int is_prime_number(int n)
 {
+	int is_prime = 0;
 
+	if (n < 2)
+		is_prime = 0;
+	else if (n == 2)
+		is_prime = 1;
+	else if (n % 2 == 1)
+		is_prime = 1;
+	return (is_prime);
 }
