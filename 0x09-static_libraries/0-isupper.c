d28b5ace5056cea233a096a0e90256486c478e5d
DebbyAsh <dilorey35@gmail.com>
2023-03-10 15:32:18 -0800

    Task 0

--- 0x09-static_libraries/0-isupper.c
+++ 0x09-static_libraries/0-isupper.c
@@ -1,15 +1,6 @@
 #include "main.h"
 
 /**
- * _isupper - function declaration
- * Description: check case of c
- * @c: character to test
- * Return: 1 for true and 0 for false
- */
-
-int _isupper(int c);
-
-/**
  * _isupper - function definition
  * Description: tests case of c
  * @c: character to test
