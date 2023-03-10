6422e4790fdf06c371fe53d5252d617fd113e557
DebbyAsh <dilorey35@gmail.com>
2023-03-10 15:39:15 -0800

    Task 0

--- 0x09-static_libraries/3-islower.c
+++ 0x09-static_libraries/3-islower.c
@@ -2,15 +2,6 @@
 #include "main.h"
 
 /**
- * _islower - tests if lower or uppercase
- * @c: character passed as integer
- * Description: c is passed  as an arguement
- * Return: void
- */
-
-int _islower(int c);
-
-/**
  * _islower - tests c if lower or upper
  * @c: character is passed as integer
  * Description: c is the arguement as integer
