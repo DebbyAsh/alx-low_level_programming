3ddeb49121563498b805c92ac6c6aac7084a872e
DebbyAsh <dilorey35@gmail.com>
2023-03-10 14:58:46 -0800

    Task 4

--- 0x0A-argc_argv/4-add.c
+++ 0x0A-argc_argv/4-add.c
@@ -26,13 +26,13 @@
 		return (1);
 	}
 
-	/**
-	 * main - Print the name of the program
-	 * @argc: Count arguments
-	 * @argv: Arguments
-	 *
-	 * Return: Always 0 (Success)
-	 */
+/**
+* main - Print the name of the program
+* @argc: Count arguments
+* @argv: Arguments
+*
+* Return: Always 0 (Success)
+*/
 
 	int main(int argc, char *argv[])
 	{
