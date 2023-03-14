4ce4a70d377bf13ab1e6a915b7b8a949583491c3
DebbyAsh <dilorey35@gmail.com>
2023-03-14 07:05:03 -0700

    Task 0

---
+++ 0x0B-malloc_free/1-strdup.c
@@ -0,0 +1,12 @@
+#include "main.h"
+
+/**
+ * _strdup - function that returns a pointer to an allocated space
+ * @str: string
+ * Return: NULL 
+ */
+
+char *_strdup(char *str)
+{
+
+}
---
+++ 0x0B-malloc_free/3-alloc_grid.c
@@ -0,0 +1,13 @@
+#include "main.h"
+
+/**
+ *alloc_grid - function that returns a pointer to a 2 dimensional array 
+ *@width: width of array
+ *@height: height of array
+ *Return: NULL
+ */
+
+int **alloc_grid(int width, int height)
+{
+
+}
