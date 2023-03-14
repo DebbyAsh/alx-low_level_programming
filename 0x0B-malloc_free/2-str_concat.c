d9b4fc4fdf664c50fea4d235251501b4f18191c5
DebbyAsh <dilorey35@gmail.com>
2023-03-14 09:14:47 -0700

    Task 2

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -1,30 +1,49 @@
-#include <stdio.h>
-#include <stdlib.h>
 #include "main.h"
+#include <stdlib.h>
 
 /**
- * _strdup - duplicate to new memory space location
- * @str: char
- * Return: 0
+ * str_concat - get ends of input and add together for size
+ * @s1: input one to concat
+ * @s2: input two to concat
+ * Return: concat of s1 and s2
  */
 
-char *_strdup(char *str)
+char *str_concat(char *s1, char *s2)
 {
+	unsigned int len1, len2, size, i, j;
+	char *nstr;
 
-	char *aaa;
-	int i, r = 0;
+	if (s1 == NULL)
+		s1 = "";
+	if (s2 == NULL)
+		s2 = "";
 
-	if (str == NULL)
+	len1 = 0;
+	while (s1[len1] != '\0')
+		len1++;
+	len2 = 0;
+	while (s2[len2] != '\0')
+		len2++;
+
+	size = len1 + len2;
+
+	nstr = malloc((sizeof(char) * size) + 1);
+
+	if (nstr == NULL)
 		return (NULL);
+
 	i = 0;
-
-	while (str[i] != '\0')
+	while (i < len1)
+	{
+		nstr[i] = s1[i];
 		i++;
-	aaa = malloc(sizeof(char) * (i + 1));
-	if (aaa == NULL)
-		return (NULL);
-
-	for (r = 0; str[r]; r++)
-		aaa[r] = str[r];
-	return (aaa);
+	}
+	j = 0;
+	while (i <= size)
+	{
+		nstr[i] = s2[j];
+		i++;
+		j++;
+	}
+	return (nstr);
 }
